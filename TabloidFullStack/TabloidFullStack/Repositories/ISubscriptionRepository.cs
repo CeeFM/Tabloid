7a8184ad58cabe6a76cda015ec3028e103ed7b97
Hannah Thompson <hannahmthompsonedu@gmail.com>
2024-02-14 10:24:53 -0500

    working back end

---
+++ TabloidFullStack/TabloidFullStack/Repositories/ISubscriptionRepository.cs
@@ -0,0 +1,9 @@
+﻿using TabloidFullStack.Models;
+
+namespace TabloidFullStack.Repositories
+{
+    public interface ISubscriptionRepository
+    {
+        void Add(Subscription subscription);
+    }
+}
--- TabloidFullStack/TabloidFullStack/Repositories/SubscriptionRepository.cs
+++ TabloidFullStack/TabloidFullStack/Repositories/SubscriptionRepository.cs
@@ -1,10 +1,35 @@
-﻿namespace TabloidFullStack.Repositories
+﻿using TabloidFullStack.Models;
+using TabloidFullStack.Utils;
+
+namespace TabloidFullStack.Repositories
 {
-    public class SubscriptionRepository : BaseRepository
+    public class SubscriptionRepository : BaseRepository, ISubscriptionRepository
     {
 
         public SubscriptionRepository(IConfiguration config) : base(config) { }
 
+        public void Add(Subscription subscription)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO Subscription (
+                            SubscriberUserProfileId, ProviderUserProfileId, BeginDateTime, EndDateTime)
+                        OUTPUT INSERTED.ID
+                        VALUES(
+                            @SubscriberUserProfileId, @ProviderUserProfileId, @BeginDateTime, @EndDateTime )";
+                    cmd.Parameters.AddWithValue("@SubscriberUserProfileId", subscription.SubscriberUserProfileId);
+                    cmd.Parameters.AddWithValue("@ProviderUserProfileId", subscription.ProviderUserProfileId);
+                    cmd.Parameters.AddWithValue("@BeginDateTime", subscription.BeginDateTime);
+                    cmd.Parameters.AddWithValue("@EndDateTime", DbUtils.ValueOrDBNull(subscription.EndDateTime));
+
+                    subscription.Id = (int)cmd.ExecuteScalar();
+                }
+            }
+        }
 
     }
 }
